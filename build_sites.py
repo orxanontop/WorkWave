038f6dabe2d230a0d375a0eecb0eceac044f9519
orxan <orxanmajidli087@gmail.com>
2026-05-07 18:25:00 +0400

    chore: remove unnecessary files and update gitignore

--- build_sites.py
+++
@@ -1,6 +0,0 @@
-import os
-def w(path,c):
-    os.makedirs(os.path.dirname(path),exist_ok=True)
-    with open(path,'w',encoding='utf-8') as f: f.write(c)
-# will write actual content via bash heredoc
-print("ready")
--- gen_html.py
+++
@@ -1,6 +0,0 @@
-import os
-dirs = ["C:/Users/majid/Desktop/WorkWave/with_skill_site",
-        "C:/Users/majid/Desktop/WorkWave/without_skill_site"]
-for d in dirs:
-    os.makedirs(d, exist_ok=True)
-print("dirs ready")
